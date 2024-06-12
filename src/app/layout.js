f77be0afbd5e936d2dee157901091c80adb071da
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-06-12 17:11:23 -0600

    cambios favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ header, navbar }) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/src/assets/img/favicon.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Sandicare" />
         <meta property="og:description" content="" />
